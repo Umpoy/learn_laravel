353de2880606c2c9258c5eb0b43c633f7e943a84
Umpoy <iumpoy@yahoo.com>
2019-03-12 12:48:47 -0700

    changed if staments to switch

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -9,17 +9,18 @@ class FrontEndController extends Controller
     public function page(Request $request)
     {
         $page_title = "Home Page";
-        
-        if($request->path() == 'about'){
-            $page_title = "About Page";
-        }
 
-        if($request->path() == 'contact'){
-            $page_title = "Contact Page";
-        }
+        switch($request->path()){
+            case 'about':
+                $page_title = "About Page";
+            break;
+            case 'contact':
+                $page_title = "Contact Page";
+            break;
+            case 'blog':
+                $page_title = "Blog Page";
+            break;
 
-        if($request->path() == 'blog'){
-            $page_title = "Blog Page";
         }
 
         return view('pages.static', ['page_title' => $page_title]);
